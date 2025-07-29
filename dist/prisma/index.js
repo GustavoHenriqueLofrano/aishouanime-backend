b191c22aef7d8dc9c1181f9cc7743c02afcb8524
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-07-29 08:49:34 -0300

    add

--- dist/prisma/index.js
+++ dist/prisma/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("../../../aishouanime-backend/src/generated/prisma/client");
+const client_1 = require("../../src/generated/prisma/client");
 const prismaClient = new client_1.PrismaClient({
     log: ['info', 'warn', 'error']
 });
