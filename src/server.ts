3b666ed4b2d83762cecc733126cefceff758a256
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-07-29 09:05:44 -0300

    add

--- src/server.ts
+++ src/server.ts
@@ -25,4 +25,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 })
 
-app.listen(process.env.PORT || 3333, () => console.log('Servidor online!'))
+app.listen(process.env.PORT , () => console.log('Servidor online!'))
