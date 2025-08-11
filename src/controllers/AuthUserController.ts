2e6060bcac81385c7c4edad34f9aade9d69a81cf
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-08-10 21:07:41 -0300

    remove

--- src/controllers/AuthUserController.ts
+++ src/controllers/AuthUserController.ts
@@ -1,11 +1,6 @@
 import {Request, Response} from 'express'
 import { AuthUserService } from '../service/AuthUserService'
 
-interface AuthUserRequest {
-    name: string;
-    password: string;
-}
-
 class AuthUserController {
     async handle(req: Request, res: Response){
         const { name, password } = req.body;
