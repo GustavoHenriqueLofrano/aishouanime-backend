5397e88a0799ab91ca857cc6d9cf54b00b5a8007
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-08-10 21:14:58 -0300

    add hash 10

--- src/service/CreateUserService.ts
+++ src/service/CreateUserService.ts
@@ -28,7 +28,7 @@ class CreateUserService {
             throw new Error("User already exists");
         }
     
-        const passwordHash = await hash(password, 12);
+        const passwordHash = await hash(password, 10);
 
     const user = await prismaClient.user.create({
       data:{
